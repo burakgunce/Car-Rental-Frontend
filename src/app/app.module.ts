9482cade11865d69edb43701d990d1181e9e8e54
burakgunce <brk.gnc9@hotmail.com>
2024-03-28 05:03:44 +0300

    angular structure has been prepared

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClient
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
